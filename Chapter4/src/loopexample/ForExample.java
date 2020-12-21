b5a883e3b64e22efb969032a2061e09f427e98b8
WonHyokJun <wonhyokjun@gmail.com>
2020-12-21 22:10:19 +0900

    For and while nested loop

--- Chapter4/src/loopexample/ForExample.java
+++ Chapter4/src/loopexample/ForExample.java
@@ -4,14 +4,35 @@ public class ForExample {
 
 	public static void main(String[] args) {
 
-		int num;
+		/*int num;
 		int sum;
 		
 		for(num = 1,sum = 0; num <= 10; num ++) {
 			sum += num;
 		}
 		System.out.println(sum);
-		System.out.println(num);
+		System.out.println(num);*/
+		
+		int dan =2;
+		int times = 1;
+		
+		/*for (dan = 2; dan <=9; dan++) {
+			
+			for (times = 1; times <=9; times++) {
+				System.out.println(dan + "X" + times + "=" + dan*times);
+			}
+			System.out.println("");
+		}*/
+		
+		while(dan <=9) {
+			times = 1;
+			while(times <=9) {
+				System.out.println(dan + "X" + times + "=" + dan*times);
+				times++;
+			}
+			dan++;
+			System.out.println("");
+		}
 	}
 
 }
