e5eae9e75d421bfd1765e055b2c7ba930fc4552c
WonHyokJun <wonhyokjun@gmail.com>
2020-12-21 21:26:24 +0900

    If statement example

---
+++ Chapter4/src/ifexample/ifExample1.java
@@ -0,0 +1,17 @@
+package ifexample;
+
+public class ifExample1 {
+
+	public static void main(String[] args) {
+
+		int age = 10;
+		
+		if(age >=8 ) {
+			System.out.println("I am in the school");
+		}
+		else {
+			System.out.println("I am not in the school");
+		}
+	}
+
+}
