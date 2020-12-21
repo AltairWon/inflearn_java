3048981e81a895941a938ea8858c6dc88824159a
WonHyokJun <wonhyokjun@gmail.com>
2020-12-21 16:50:01 +0900

    Variables

---
+++ Chapter2/src/chapter2/VariableEx2.java
@@ -0,0 +1,13 @@
+package chapter2;
+
+public class VariableEx2 {
+
+	public static void main(String[] args) {
+		
+		int num = 10;
+		int level = 50;
+		long num2 = 12345678900L; //저장을 할때 8바이트짜리로 저장을 하기 위해서 L을 넣는다
+		
+	}
+
+}
