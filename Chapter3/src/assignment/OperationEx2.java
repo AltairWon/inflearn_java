004b5760a8a5d9bd8cd5a48f8e24550dc83f0eb6
WonHyokJun <wonhyokjun@gmail.com>
2020-12-21 17:49:56 +0900

    Opeartion Example 2

---
+++ Chapter3/src/assignment/OperationEx2.java
@@ -0,0 +1,22 @@
+package assignment;
+
+public class OperationEx2 {
+
+	public static void main(String[] args) {
+		
+		/*int num = 10;
+		
+		System.out.println(++num); // ++num = num + 1 first
+		
+		System.out.println(num++); // num++ = num + first later
+		
+		System.out.println(num); // num = num+1
+		*/
+		System.out.println(3<5);
+		int num1 = 10;
+		int num2 = 5;
+		boolean flag = (num1>num2);
+		System.out.println(flag);
+	}
+
+}
