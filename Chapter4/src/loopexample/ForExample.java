edaa2b1b3c5313ae7d7cfdeee50bb41912fc39a5
WonHyokJun <wonhyokjun@gmail.com>
2020-12-21 22:23:41 +0900

    Update For example

--- Chapter4/src/loopexample/ForExample.java
+++ Chapter4/src/loopexample/ForExample.java
@@ -25,8 +25,15 @@ public class ForExample {
 		}*/
 		
 		while(dan <=9) {
+			/*if((dan % 2 != 0)) { // 짝수 구구단만
+				dan++;
+				continue;
+			}*/
 			times = 1;
 			while(times <=9) {
+				if(times > dan) { // dan만큼 곱하기
+					break;
+				}
 				System.out.println(dan + "X" + times + "=" + dan*times);
 				times++;
 			}
