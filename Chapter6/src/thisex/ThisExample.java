c2a54cd38da91e12d7c78a20d345bda3a93b9cb6
WonHyokJun <wonhyokjun@gmail.com>
2020-12-23 20:56:39 +0900

    This example.

---
+++ Chapter6/src/thisex/ThisExample.java
@@ -0,0 +1,32 @@
+package thisex;
+
+class BirthDay{
+	int day;
+	int month;
+	int year;
+	
+	public void setYear(int year) {
+		this.year = year; //이름이 같을 변수명이면 this.year로 선언을 하는 것이 좋다.
+						  //this = 자기 자신의 메모리를 가리킴. 생성된 메모리의 주소를 가리킴.
+	}
+	
+	public void printThis() {
+		System.out.println(this); // this = 현재 instance 한 주소값
+	}
+}
+
+public class ThisExample {
+
+	public static void main(String[] args) {
+		
+		BirthDay b1 = new BirthDay();
+		BirthDay b2 = new BirthDay();
+		
+		System.out.println(b1);
+		b1.printThis();
+		
+		System.out.println(b2);
+		b2.printThis();
+	}
+
+}
