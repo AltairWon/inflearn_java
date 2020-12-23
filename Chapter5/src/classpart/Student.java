2257de7b403e62ae3956668c199f0b7cce285b0c
WonHyokJun <wonhyokjun@gmail.com>
2020-12-23 16:49:08 +0900

    Constructor Overload

--- Chapter5/src/classpart/Student.java
+++ Chapter5/src/classpart/Student.java
@@ -7,6 +7,15 @@ public class Student {
 	int grade;
 	String address;
 	
+	public Student() {} //생성자 overload constructor overload
+						// default constructor 잘 기억하자
+	
+	public Student(int id, String name) { //constructor 생성자가 하나도 없는 경우는 support 할 수 있지만
+		studentID = id;					//그렇지않으면 support 하지 않는다.
+		studentName = name;
+		
+	}
+	
 	public void showStudentInfor() {
 		System.out.println(studentName + ", " + address);
 	}
@@ -18,5 +27,4 @@ public class Student {
 	public void setStudentName(String name) { //no return value but has void and String name
 		studentName = name;
 	}
-
 }
--- Chapter5/src/classpart/StudentTest.java
+++ Chapter5/src/classpart/StudentTest.java
@@ -2,12 +2,22 @@ package classpart;
 
 public class StudentTest {
 	
-	public static void main(String[] args) {
-	
-	Student studentLee = new Student();
-	studentLee.studentName = "Lee Soon Shin";
-	studentLee.address = "Seoul SeoCho";
-	
-	studentLee.showStudentInfor();
+	public static void main(String[] args) {			
+			
+		Student studentLee = new Student(100, "Lee Soon Shin"); //생성자가 있는 경우
+		studentLee.studentName = "Lee Soon Shin";
+		studentLee.studentID = 100;
+		studentLee.address = "Seoul";
+			
+		Student studentKim = new Student(); //생성자가 없는 경우
+		studentKim.studentName = "Kim Yoon Shin";
+		studentKim.studentID = 101;
+		studentKim.address = "Incheon";
+			
+		studentLee.showStudentInfor();
+		studentKim.showStudentInfor(); 
+		
+		System.out.println(studentLee); //reference value
+		System.out.println(studentKim);
 	}
 }
