29525187e8aed2c384fe8238318c6d41ce26217b
WonHyokJun <wonhyokjun@gmail.com>
2020-12-23 21:07:37 +0900

    Another This example.

--- Chapter6/src/thisex/CallAnotherConst.java
+++ Chapter6/src/thisex/CallAnotherConst.java
@@ -9,6 +9,8 @@ class Person{
 		//name = "no name";
 		//age = 1;
 		
+		//int i = 0; // this를 선언한 메소드 안에다가는 어떤것도 생성이 불가능하다.
+		
 		this("No name", 1); // this 를 생성이 되었기 때문에 사용이 가능하다.
 	}
 	
@@ -16,6 +18,11 @@ class Person{
 		this.name = name;
 		this.age = age;
 	}
+	
+	public Person returnSelf() { //자기 자신의 class명으로.
+		return this; //현재 instance 한 주소값이 된다. 
+	}
+	
 }
 
 
@@ -27,6 +34,8 @@ public class CallAnotherConst {
 		
 		Person p1 = new Person();
 		System.out.println(p1.name);
+		
+		System.out.println(p1.returnSelf()); //주소값이 출력이 된 것을 볼 수 있다.
 	}
 
 }
