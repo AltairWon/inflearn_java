b39a73a4d781a25413eea6501e6760f651376404
WonHyokJun <wonhyokjun@gmail.com>
2021-01-11 22:55:58 +0900

    Edited file

--- Chapter10/src/scheduler/SchedulerTest.java
+++ Chapter10/src/scheduler/SchedulerTest.java
@@ -7,7 +7,7 @@ public class SchedulerTest {
 	public static void main(String[] args) throws IOException {
 		
 		// R, L, P
-		System.out.println("전화 상담 배분방식을 선택하세요, R, L P");
+		System.out.println("전화 상담 배분방식을 선택하세요, R L P");
 		
 		int ch  = System.in.read(); //integer 파일로 받아서 읽는다!
 		Scheduler scheduler = null;
